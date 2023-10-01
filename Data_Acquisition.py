f7f2bc4fce7b5f9dd84f4e6d3be769b4c9260c0f
YuyangX <yuyangxiong23@gmail.com>
2023-09-30 23:35:49 -0400

    Create Data_Acquisition.py
    This file can successfully download all .json files from nhl website, but it still exists defect, the files that don't exist will also be downloaded.

---
+++ Data_Acquisition.py
@@ -0,0 +1,81 @@
+import requests   
+from pathlib import Path     
+import json       
+
+def compose_4digit_game_id (game):
+    if game < 10:
+        game = "000" + str(game)
+    elif game < 100:
+        game = "00" + str(game)
+    elif game < 1000:
+        game = "0" + str(game)
+    else:
+        game = str(game)
+    return game
+
+def compose_regSeason_game_id (year, game):
+    return str(year) + "02" + str(compose_4digit_game_id(game))
+
+def compose_playoff_game_id (year, round, matchup, game):
+    return str(year) + "030" + str(round) + str(matchup) + str(game)
+
+def compose_api_url (game_id):
+    return "https://statsapi.web.nhl.com/api/v1/game/" + game_id + "/feed/live/"
+
+def get_game_data (api_url):
+    response = requests.get(api_url)
+    return response.json()
+
+def create_json_file (year, type, round, matchup, game):
+    game_id = None
+    if type == "02":
+        game_id = compose_regSeason_game_id(year, game)
+    else:
+        game_id = compose_playoff_game_id(year, round, matchup, game)
+
+    file_path = "./" + str(year) + "/" + str(type) + "/" + game_id + ".json" 
+    if not Path(file_path).exists():
+        with open(file_path, "w") as outfile:
+            json.dump(get_game_data(compose_api_url(game_id)), outfile)
+
+def __main__ ():
+    for year in range(2016, 2021):
+        print(year)
+        # type 02 is regular season, type 03 is playoff
+        for type in ["02", "03"]:
+            print(type)
+            if type == "02":
+                if year == 2016:
+                    Path("./2016/02").mkdir(parents=True, exist_ok=True)
+                    for game in range(1, 1231):
+                        create_json_file(year, type, None, None, game)
+                elif 2017 <= year <= 2019:
+                    Path("./" + str(year) + "/02").mkdir(parents=True, exist_ok=True)    
+                    for game in range(1, 1272):
+                        create_json_file(year, type, None, None, game)
+                else: 
+                    Path("./" + str(year) + "/02").mkdir(parents=True, exist_ok=True)    
+                    for game in range(1, 868):
+                        create_json_file(year, type, None, None, game)
+                                        
+            else: 
+                Path("./" + str(year) + "/03").mkdir(parents=True, exist_ok=True)
+                for round in range(1, 5):
+                    if round == 1:
+                        for matchup in range(1,8):
+                            for game in range(1,8):
+                                create_json_file(year, type, round, matchup, game)
+                    elif round == 2:
+                        for matchup in range(1,5):
+                            for game in range(1,8):
+                                create_json_file(year, type, round, matchup, game)
+                    elif round == 3:
+                        for matchup in range(1,3):
+                            for game in range(1,8):
+                                create_json_file(year, type, round, matchup, game)
+                    else:
+                        for game in range(1,8):
+                            create_json_file(year, type, round, matchup, game)
+                        
+if __name__ == "__main__":
+    __main__()
