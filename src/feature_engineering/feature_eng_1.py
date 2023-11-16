800da769671c2bf2198e4aecca93341fddcccb84
YuyangX <yuyangxiong23@gmail.com>
2023-11-16 13:42:11 -0500

    Update feature_eng_1.py
    delete jointplot

--- src/feature_engineering/feature_eng_1.py
+++ src/feature_engineering/feature_eng_1.py
@@ -209,65 +209,38 @@ folder_path_20192020R = data_folder  + '2019-2020/R'
 folder_path_20202021P = data_folder  + '2020-2021/P'
 folder_path_20202021R = data_folder  + '2020-2021/R'
 
-# create_csv(folder_path_20162017P, save_dir)
-# create_csv(folder_path_20162017R, save_dir)
-# create_csv(folder_path_20172018P, save_dir)
-# create_csv(folder_path_20172018R, save_dir)
-# create_csv(folder_path_20182019P, save_dir)
-# create_csv(folder_path_20182019R, save_dir)
-# create_csv(folder_path_20192020P, save_dir)
-# create_csv(folder_path_20192020R, save_dir)
+create_csv(folder_path_20162017P, save_dir)
+create_csv(folder_path_20162017R, save_dir)
+create_csv(folder_path_20172018P, save_dir)
+create_csv(folder_path_20172018R, save_dir)
+create_csv(folder_path_20182019P, save_dir)
+create_csv(folder_path_20182019R, save_dir)
+create_csv(folder_path_20192020P, save_dir)
+create_csv(folder_path_20192020R, save_dir)
 
-# df20162017R = pd.read_csv(save_dir+'2016-2017_R_clean.csv')
-# df20162017P = pd.read_csv(save_dir+'2016-2017_P_clean.csv')
-# df20162017 = pd.concat([df20162017R,df20162017P])
-# df20162017.to_csv(save_dir+'20162017_clean.csv', encoding='utf-8', index=False)
+df20162017R = pd.read_csv(save_dir+'2016-2017_R_clean.csv')
+df20162017P = pd.read_csv(save_dir+'2016-2017_P_clean.csv')
+df20162017 = pd.concat([df20162017R,df20162017P])
+df20162017.to_csv(save_dir+'20162017_clean.csv', encoding='utf-8', index=False)
 
-# df20172018R = pd.read_csv(save_dir+'2017-2018_R_clean.csv')
-# df20172018P = pd.read_csv(save_dir+'2017-2018_P_clean.csv')
-# df20172018 = pd.concat([df20172018R,df20172018P])
-# df20172018.to_csv(save_dir+'20172018_clean.csv', encoding='utf-8', index=False)
+df20172018R = pd.read_csv(save_dir+'2017-2018_R_clean.csv')
+df20172018P = pd.read_csv(save_dir+'2017-2018_P_clean.csv')
+df20172018 = pd.concat([df20172018R,df20172018P])
+df20172018.to_csv(save_dir+'20172018_clean.csv', encoding='utf-8', index=False)
 
-# df20182019R = pd.read_csv(save_dir+'2018-2019_R_clean.csv').sort_values(by='gamePk', ascending=True)
-# df20182019P = pd.read_csv(save_dir+'2018-2019_P_clean.csv').sort_values(by='gamePk', ascending=True)
-# df20182019 = pd.concat([df20182019R,df20182019P]).sort_values(by='gamePk', ascending=True)
-# df20182019.to_csv(save_dir+'20182019_clean.csv', encoding='utf-8', index=False)
+df20182019R = pd.read_csv(save_dir+'2018-2019_R_clean.csv').sort_values(by='gamePk', ascending=True)
+df20182019P = pd.read_csv(save_dir+'2018-2019_P_clean.csv').sort_values(by='gamePk', ascending=True)
+df20182019 = pd.concat([df20182019R,df20182019P]).sort_values(by='gamePk', ascending=True)
+df20182019.to_csv(save_dir+'20182019_clean.csv', encoding='utf-8', index=False)
 
-# df20192020R = pd.read_csv(save_dir+'2019-2020_R_clean.csv').sort_values(by='gamePk', ascending=True)
-# df20192020P = pd.read_csv(save_dir+'2019-2020_P_clean.csv').sort_values(by='gamePk', ascending=True)
-# df20192020 = pd.concat([df20192020R,df20192020P]).sort_values(by='gamePk', ascending=True)
-# df20192020.to_csv(save_dir+'20192020_clean.csv', encoding='utf-8', index=False)
+df20192020R = pd.read_csv(save_dir+'2019-2020_R_clean.csv').sort_values(by='gamePk', ascending=True)
+df20192020P = pd.read_csv(save_dir+'2019-2020_P_clean.csv').sort_values(by='gamePk', ascending=True)
+df20192020 = pd.concat([df20192020R,df20192020P]).sort_values(by='gamePk', ascending=True)
+df20192020.to_csv(save_dir+'20192020_clean.csv', encoding='utf-8', index=False)
 
-# df20162018 = pd.concat([df20162017 ,df20172018]).sort_values(by='gamePk', ascending=True)
-# df20162019 = pd.concat([df20162018 ,df20182019]).sort_values(by='gamePk', ascending=True)
-# df20162019 = pd.concat([df20162019 ,df20192020]).sort_values(by='gamePk', ascending=True)
-# df20162019.to_csv(save_dir+'20162019_clean.csv', encoding='utf-8', index=False)
-
-
-df2016 = pd.read_csv("data/FE1/20162017_clean.csv")
-df2017 = pd.read_csv("data/FE1/20172018_clean.csv")
-df2018 = pd.read_csv("data/FE1/20182019_clean.csv")
-df2019 = pd.read_csv("data/FE1/20192020_clean.csv")
-df = pd.concat([df2016, df2017, df2018, df2019])
-
-cleaned_df =  df[df['distance']!='shoot out']
-
-cleaned_df['distance'] = pd.to_numeric(cleaned_df['distance'], errors='coerce')
-cleaned_df['angle'] = pd.to_numeric(cleaned_df['angle'], errors='coerce')
-cleaned_df['distance']=cleaned_df.distance.astype('float')
-cleaned_df['angle']=cleaned_df.angle.astype('float')
-cleaned_df = cleaned_df[(~cleaned_df.distance.isna())]
+df20162018 = pd.concat([df20162017 ,df20172018]).sort_values(by='gamePk', ascending=True)
+df20162019 = pd.concat([df20162018 ,df20182019]).sort_values(by='gamePk', ascending=True)
+df20162019 = pd.concat([df20162019 ,df20192020]).sort_values(by='gamePk', ascending=True)
+df20162019.to_csv(save_dir+'20162019_clean.csv', encoding='utf-8', index=False)
 
 
-plot_df = cleaned_df.reset_index(drop=True).copy()
-cleaned_df.info()
-
-fig = plt.figure(figsize=(20, 20))
-        
-ax = sns.jointplot(data=plot_df[['distance', 'angle']], x="distance", y="angle", kind="hist", ratio=4, marginal_kws=dict(bins=50),joint_kws=dict(bins=50))
-ax.fig.suptitle('2D Histogram of shots by angle and distance from net')
-ax.fig.tight_layout()
-ax.ax_joint.set_xlabel("Shot Distance (feet)")
-ax.ax_joint.set_ylabel("Shot Angle (Degree)")
-ax.savefig(os.path.join("assets/images/2Dhist_shot_angle_distance.png"))  
-plt.show()
