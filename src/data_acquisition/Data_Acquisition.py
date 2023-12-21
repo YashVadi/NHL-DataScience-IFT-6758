78acfb965fe9ff5cd3f2d37e4d3a84139a62d398
YuyangX <yuyangxiong23@gmail.com>
2023-12-21 17:08:00 -0500

    Update Data_Acquisition.py
    update Data_acquisition.py according to the new api, and change the path to which the json files are stored

--- src/data_acquisition/Data_Acquisition.py
+++ src/data_acquisition/Data_Acquisition.py
@@ -20,7 +20,7 @@ def compose_playoff_game_id (year, round, matchup, game):
     return str(year) + "030" + str(round) + str(matchup) + str(game)
 
 def compose_api_url (game_id):
-    return "https://statsapi.web.nhl.com/api/v1/game/" + game_id + "/feed/live/"
+    return "https://api-web.nhle.com/v1/gamecenter/" + game_id + "/play-by-play"
 
 def get_game_data (api_url):
     response = requests.get(api_url)
@@ -33,38 +33,39 @@ def create_json_file (year, type, round, matchup, game):
     else:
         game_id = compose_playoff_game_id(year, round, matchup, game)
 
-    file_path = "./data/" + str(year) + "-" + str(year+1) + "/" + str(type) + "/" + game_id + ".json" 
+    file_path = "./ift6758/ift6758/data/data_json/" + str(year) + "-" + str(year+1) + "/" + str(type) + "/" + game_id + ".json" 
     if not Path(file_path).exists():
-        json_data = get_game_data(compose_api_url(game_id))
         try:
-            if json_data.get("gameData").get("status").get("abstractGameState") == "Final":
+            json_data = get_game_data(compose_api_url(game_id))
+
+            if json_data.get("gameState") == "OFF":
                 with open(file_path, "w") as outfile:    
                     json.dump(json_data, outfile)
         except:
             print("No game data for " + game_id)
 
 def __main__ ():
-    for year in range(2016, 2021):
+    for year in range(2019, 2023):
         print(year)
         # type 02 is regular season, type 03 is playoff
         for type in ["R", "P"]:
             print(type)
             if type == "R":
-                if year == 2016:
-                    Path("./data/2016-2017/R").mkdir(parents=True, exist_ok=True)
-                    for game in range(1, 1231):
+                if year == 2019:
+                    Path("./ift6758/ift6758/data/data_json/" + str(year) + "-" + str(year+1) + "/R").mkdir(parents=True, exist_ok=True)     
+                    for game in range(1, 1083):
                         create_json_file(year, type, None, None, game)
-                elif 2017 <= year <= 2019:
-                    Path("./data/" + str(year) + "-" + str(year+1) + "/R").mkdir(parents=True, exist_ok=True)    
-                    for game in range(1, 1272):
+                elif year == 2020: 
+                    Path("./ift6758/ift6758/data/data_json/" + str(year) + "-" + str(year+1) + "/R").mkdir(parents=True, exist_ok=True)    
+                    for game in range(1, 869):
                         create_json_file(year, type, None, None, game)
-                else: 
-                    Path("./data/" + str(year) + "-" + str(year+1) + "/R").mkdir(parents=True, exist_ok=True)    
-                    for game in range(1, 868):
+                elif 2021 == year or year == 2022:
+                    Path("./ift6758/ift6758/data/data_json/" + str(year) + "-" + str(year+1) + "/R").mkdir(parents=True, exist_ok=True)    
+                    for game in range(1, 1313):
                         create_json_file(year, type, None, None, game)
                                         
             else: 
-                Path("./data/" + str(year) + "-" + str(year+1) + "/P").mkdir(parents=True, exist_ok=True)
+                Path("./ift6758/ift6758/data/data_json/" + str(year) + "-" + str(year+1) + "/P").mkdir(parents=True, exist_ok=True)
                 for round in range(1, 5):
                     if round == 1:
                         for matchup in range(1,9):
