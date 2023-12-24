b668e83f576233cea23919c19573a62136c6b28f
YuyangX <yuyangxiong23@gmail.com>
2023-12-23 22:20:12 -0500

    Update game_client.py
    add home_or_away in dataframe, add reference to original codes

--- serving/game_client.py
+++ serving/game_client.py
@@ -8,11 +8,11 @@ import math
 logger = logging.getLogger(__name__)
 
 #removed if not so that tracker won't accumulate after each run of app
-# with open('tracker.json', 'w') as outfile:
-#     history = {}
-#     json.dump(history, outfile)
+with open('tracker.json', 'w') as outfile:
+    history = {}
+    json.dump(history, outfile)
 
-class Game_Client:
+class GameClient:
     def __init__(self):
         logger.info(f"Initializing ClientGame...")
 
@@ -93,6 +93,11 @@ class Game_Client:
                     angle = self.get_angle(shooterTeamId,homeId,homeSide,x,y)
                     isGoal = 0
                     emptyNet = 0
+                    if shooterTeamId == homeId:
+                        home_or_away = 'home'
+                    else:
+                        home_or_away = 'away'
+                    
                     if  plays[i]['typeDescKey'] ==  "goal":
                         isGoal = 1
                         if shooterTeamId == homeId:
@@ -104,7 +109,7 @@ class Game_Client:
                             if int(plays[i].get('situationCode')[3]) == 0:
                                 emptyNet = 1
 
-                    new_row = {'gameId':ID,'distance':distance, 'angle':angle, 'emptyNet':emptyNet,'isGoal':isGoal,' period': period, 'timeRemaining':timeRemaining, 'home_score':home_score, 'away_score':away_score}
+                    new_row = {'gameId':ID, 'home_or_away': home_or_away, 'distance':distance, 'angle':angle, 'emptyNet':emptyNet,'isGoal':isGoal,' period': period, 'timeRemaining':timeRemaining, 'home_score':home_score, 'away_score':away_score}
                     data_list.append(new_row)
         df = pd.DataFrame(data_list)    
         return df       
@@ -127,6 +132,10 @@ class Game_Client:
         period = df[' period'].values[-1]
         timeRemaining = df['timeRemaining'].values[-1]
         
+        ###############################################
+        # The codes below are refered to github:
+        # "https://github.com/haooyuee/nhl-shot-analysis/blob/main/milestone_3_nhl_app/ift6758/ift6758/client/game_client.py"
+        ###############################################
         tracker = open('tracker.json')
         history = json.load(tracker)
         previous_idx = 0
@@ -138,12 +147,14 @@ class Game_Client:
             history[str(game_id)]['idx'] = len(df)
         with open('tracker.json', 'w') as outfile:
             json.dump(history, outfile) 
-        df = df.reset_index().drop('index', axis=1)[previous_idx:]        
+        df = df.reset_index().drop('index', axis=1)[previous_idx:]   
+        ###############################################
+        ###############################################     
 
         return df, live, game_id, home_team, away_team, home_score, away_score, period, timeRemaining
     
 # if __name__ == "__main__":
-#     game_client = Game_Client()
+#     game_client = GameClient()
 #     df, live, game_id, home_team, away_team, home_score, away_score, period, timeRemaining = game_client.ping_game('2023020510')
 
 #     print(df)
